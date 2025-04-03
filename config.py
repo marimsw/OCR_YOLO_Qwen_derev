0d307de5d3aeb6be06102909b3ed662bec2f795a
marimsw <ai198y3@gmail.com>
2025-04-03 18:46:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------------------------
+# s3 config
+#-----------------------------------------------------------------------------------------
+DETECTORS_PATH = 'moderate/test_server/detectors/yolo/src/' # Указать полный путь!!!!!
+
+AWS_ACCESS_KEY_ID = 'secret'
+AWS_SECRET_ACCESS_KEY = 'secret'
+S3_ENDPOINT_URL = 'https://imbalanced.tech/'
+
+BUCKET_NAME = 'public'
+
+#-----------------------------------------------------------------------------------------
+# настройка путей
+#-----------------------------------------------------------------------------------------
+LOCAL_WEIGHTS_DIR = './yolo/weights/'
+LOCAL_DETECTORS_DIR = './yolo/detectors/'
+TEXTS_DIR = './texts/'
+MIXER_WEIGHTS_DIR = './mixer/weights/'
+
+
